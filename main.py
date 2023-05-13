a21a2a0523e1f424196b59317c9a89f3c0c5b823
amethystruf2 <131732343+amethystruf2@users.noreply.github.com>
2023-05-12 23:55:51 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+import os
+os.system("echo a") #not required
+os.system("firefox") #sends the command "firefox" in the linux cmd bar. oh hi!! okay if you don't see this I'll show you the other way <3
